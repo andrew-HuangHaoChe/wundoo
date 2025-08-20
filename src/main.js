2e43c91f9f1952b59edb62d640a45964ddb21e91
Huang Hao Che <62294412+andrew-HuangHaoChe@users.noreply.github.com>
2025-08-20 11:23:34 +0800

    feat: migrate main page to vue3 with shadcn

--- src/main.js
+++ src/main.js
@@ -1,64 +1,10 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios' // 主要AJAX套件
-import VueAxios from 'vue-axios' // 將它轉為Vue套件
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import 'bootstrap'
-// 客製化textarea
-import CKEditor from '@ckeditor/ckeditor5-vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import VuejsClipper from 'vuejs-clipper'
-import GoogleAuth from '@/config/google_oAuth.js'
-import {
-  Swiper,
-  Navigation,
-  Pagination,
-  Scrollbar,
-  EffectCoverflow
-} from 'swiper/js/swiper.esm.js'
-import 'swiper/css/swiper.css'
-const gauthOption = {
-  clientId: '794338067396-qqnlrcf60fvgoqn81547kmbshk70nerm.apps.googleusercontent.com',
-  scope: 'profile email',
-  prompt: 'select_account'
-}
-Swiper.use([Navigation, Pagination, Scrollbar, EffectCoverflow])
-Vue.prototype.$axios = axios
-Vue.use(VueAwesomeSwiper)
-Vue.use(CKEditor)
-Vue.use(VuejsClipper)
-Vue.use(GoogleAuth, gauthOption)
-Vue.use(VueAxios, axios)
-Vue.use(Vuex)
-axios.defaults.withCredentials = true
-Vue.config.productionTip = false
-Vue.prototype.$axios = axios
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+import axios from 'axios'
+import './assets/main.css'
 
-router.beforeEach((to, from, next) => {
-  console.log(to.fullPath)
-  const formData = new FormData()
-  formData.append('uri', to.fullPath)
-  formData.append('token', localStorage.getItem('accessToken'))
-  if (to.meta.requiresAuth) {
-    axios.post('/apipath/api/flow', formData).then((res) => {
-      console.log(res)
-      if (res.data.status) {
-        next()
-      } else {
-        alert(res.data.msg + '!')
-        next({
-          path: '/redirectLogin'
-        })
-      }
-    })
-  } else {
-    next()
-  }
-})
+const app = createApp(App)
+app.config.globalProperties.$axios = axios
+app.use(router)
+app.mount('#app')
